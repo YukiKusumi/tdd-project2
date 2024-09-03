d7416268f312e644b33e32406461e51519e7d3fe
YukiKusumi <79245623+YukiKusumi@users.noreply.github.com>
2024-09-03 22:47:53 +0900

    第8章終了

--- src/Money.php
+++ src/Money.php
@@ -1,9 +1,11 @@
 <?php
 namespace Yukikusumi\MyTddProject2;
 
-class Money
+abstract class Money
 {
     protected int $amount;
+
+    abstract public function times(int $multiplier): Money;
 
     public function equals(self $other): bool
     {
@@ -13,4 +15,13 @@ class Money
         return $this->amount === $other->amount;
     }
 
+    public static function dollar(int $amount): Money
+    {
+        return new Dollar($amount);
+    }
+
+    public static function franc(int $amount): Money
+    {
+        return new Franc($amount);
+    }
 }
