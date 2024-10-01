2cdd9cb4ba875e01b5fb4a1057e5f55910c21956
YukiKusumi <79245623+YukiKusumi@users.noreply.github.com>
2024-10-01 17:45:21 +0900

    第11章終了

--- src/Dollar.php
+++ src/Dollar.php
@@ -4,8 +4,4 @@ namespace Yukikusumi\MyTddProject2;
 
 class Dollar extends Money
 {
-    public function __construct(int $amount, string $currency)
-    {
-        parent::__construct($amount, $currency);
-    }
 }
--- src/Franc.php
+++ src/Franc.php
@@ -4,8 +4,4 @@ namespace Yukikusumi\MyTddProject2;
 
 class Franc extends Money
 {
-    public function __construct(int $amount, string $currency)
-    {
-        parent::__construct($amount, $currency);
-    }
 }
