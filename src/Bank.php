f410b04bbc58add663ee041bf2f3e4d528bedd42
YukiKusumi <79245623+YukiKusumi@users.noreply.github.com>
2024-10-01 18:39:27 +0900

    第13章終了

--- src/Bank.php
+++ src/Bank.php
@@ -6,12 +6,6 @@ class Bank
 {
     public function reduce(Expression $source, string $to):Money
     {
-        if($source instanceof Money)
-        {
-            return $source;
-        }
-
-        $sum = $source;
-        return $sum->reduce($to);
+        return $source->reduce($to);
     }
 }
